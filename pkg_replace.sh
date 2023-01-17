33dc417f9d47aa6487d3daf719245705416e3a7f
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2023-01-17 16:11:15 +0900

    Fix trap action.

--- pkg_replace.sh
+++ pkg_replace.sh
@@ -1599,6 +1599,7 @@ main() {
 
 	[ ${opt_depends} -ge 2 ] && {
 		info "'-dd' or '-RR' option set, this mode is slow!" &&
+		{ istrue "${opt_cleandeps}" && remove_dir "${PKG_REPLACE_DB_DIR}"; } &&
 		create_dir ${PKG_REPLACE_DB_DIR} &&
 		set_signal_exit=${set_signal_exit}'istrue "${opt_cleandeps}" && remove_dir "${PKG_REPLACE_DB_DIR}";';
 	}
